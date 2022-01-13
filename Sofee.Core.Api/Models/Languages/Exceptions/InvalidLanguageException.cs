1bd5f8b48b6ac0bc026042c79a37037afc4715d9
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-01-12 19:33:06 -0800

    FOUNDATIONS: Add new exception

---
+++ Sofee.Core.Api/Models/Languages/Exceptions/InvalidLanguageException.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Xeptions;
+
+namespace Sofee.Core.Api.Models.Languages.Exceptions
+{
+    public class InvalidLanguageException : Xeption
+    {
+        public InvalidLanguageException()
+            : base(message: "Language is invalid. Please, correct the error and try again.")
+        { }
+    }
+}
