227b34aa374df73a67b331a75b9b417dcd9bcd62
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-01-12 19:33:27 -0800

    FOUNDATIONS: Add validate method

--- Sofee.Core.Api/Services/Foundations/Languages/LanguageService.Validations.cs
+++ Sofee.Core.Api/Services/Foundations/Languages/LanguageService.Validations.cs
@@ -3,6 +3,7 @@
 // FREE TO USE FOR THE WORLD
 // -------------------------------------------------------
 
+using System;
 using Sofee.Core.Api.Models.Languages;
 using Sofee.Core.Api.Models.Languages.Exceptions;
 
@@ -23,5 +24,21 @@ namespace Sofee.Core.Api.Services.Foundations.Languages
             }
         }
 
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+{
+            var invalidPostException = new InvalidLanguageException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidPostException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidPostException.ThrowIfContainsErrors();
+        }
     }
 }
